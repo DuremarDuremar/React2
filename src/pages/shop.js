2bb3f2bb7ffa9ecf77d7183ea64e5d5a185990de
DuremarDuremar <duremar98@yandex,ru>
2020-10-27 17:15:01 +0300

    react

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,14 +1,50 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { sortBy } from "lodash";
+import classNames from "classnames";
 import "./shop.scss";
 
 const Shop = ({ films }) => {
   const [stateShop, setStateShop] = useState("All");
 
-  console.log(stateShop);
+  const [stateArrow, setStateArrow] = useState(true);
+
+  const classI =
+    stateArrow === true ? "fas fa-arrow-down fa-xs" : "fas fa-arrow-up fa-xs";
+
+  const changeArrow = (e) => {
+    if (stateShop === e.id) {
+      setStateArrow(!stateArrow);
+    }
+
+    console.log(e);
+    console.log(stateArrow);
+    setStateShop(e.id);
+
+    // e.classList.toggle("activeLi");
+
+    // if ((e.id = stateShop)) {
+    //   setStateArrow(
+    //     stateArrow === "fas fa-arrow-up"
+    //       ? "fas fa-arrow-down"
+    //       : "fas fa-arrow-up"
+    //   );
+    // }
+  };
+
+  // const liClasses = classNames({
+  //   if(stateShop = this.title) {
+  //     return "activeLi";
+  //   },
+  // });
+
+  // console.log(stateShop);
 
   const filmsYear = sortBy(films, ["year"]);
+  const filmsPrice = sortBy(films, ["price"]);
+  const filmsCountry = sortBy(films, ["country"]);
+
+  // console.log("filmsPrice", sortBy(films, ["price"]).reverse());
 
   return (
     <div className="shop">
@@ -16,10 +52,33 @@ const Shop = ({ films }) => {
         <div className="shop__nav">
           <h4>Catagories</h4>
           <ul>
-            <li onClick={() => setStateShop("All")}>All</li>
-            <li onClick={() => setStateShop("Year")}>Year</li>
-            <li onClick={() => setStateShop("Country")}>Country</li>
-            <li onClick={() => setStateShop("Price")}>Price</li>
+            <li
+              onClick={() => setStateShop("All")}
+              className={stateShop === "All" ? "activeLi" : null}
+            >
+              All <i className="fas fa-infinity"></i>
+            </li>
+            <li
+              onClick={(e) => changeArrow(e.currentTarget)}
+              className={stateShop === "Year" ? "activeLi" : null}
+              id="Year"
+            >
+              Year <i className={classI}></i>
+            </li>
+            <li
+              onClick={(e) => changeArrow(e.currentTarget)}
+              className={stateShop === "Country" ? "activeLi" : null}
+              id="Country"
+            >
+              Country <i className={classI}></i>
+            </li>
+            <li
+              onClick={(e) => changeArrow(e.currentTarget)}
+              className={stateShop === "Price" ? "activeLi" : null}
+              id="Price"
+            >
+              Price <i className={classI}></i>
+            </li>
           </ul>
         </div>
       </div>
@@ -44,7 +103,7 @@ const Shop = ({ films }) => {
           </form>
         </div>
       </div>
-      {films && stateShop == "All" && (
+      {films && stateShop === "All" && (
         <div className="shop__content">
           {films.map((film) => (
             <div key={film.id} className="shop__content_item">
@@ -55,7 +114,7 @@ const Shop = ({ films }) => {
           ))}
         </div>
       )}
-      {films && stateShop == "Year" && (
+      {films && stateShop === "Year" && (
         <div className="shop__content">
           {filmsYear.map((film) => (
             <div key={film.id} className="shop__content_item">
@@ -66,11 +125,27 @@ const Shop = ({ films }) => {
           ))}
         </div>
       )}
-      {films && stateShop == "Country" && (
-        <div className="shop__content">{films.map((film) => film.country)}</div>
+      {films && stateShop === "Country" && (
+        <div className="shop__content">
+          {filmsCountry.map((film) => (
+            <div key={film.id} className="shop__content_item">
+              <h3>{film.country}</h3>
+              <img src={film.image} alt={film.title} />
+              <div className="shop__content_price">{film.price} $</div>
+            </div>
+          ))}
+        </div>
       )}
-      {films && stateShop == "Price" && (
-        <div className="shop__content">{films.map((film) => film.price)}</div>
+      {films && stateShop === "Price" && (
+        <div className="shop__content">
+          {filmsPrice.map((film) => (
+            <div key={film.id} className="shop__content_item">
+              <h3>{film.price}$</h3>
+              <img src={film.image} alt={film.title} />
+              <div className="shop__content_price">{film.price} $</div>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
