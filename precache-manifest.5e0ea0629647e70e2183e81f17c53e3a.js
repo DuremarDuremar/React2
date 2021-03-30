2131b5651bdf6dcf1f7df912ae8e2fa6f9d6fbe2
Ivan <duremar98@yandex.ru>
2021-03-30 22:16:03 +0300

    Updates

---
+++ precache-manifest.5e0ea0629647e70e2183e81f17c53e3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d251f1fca16935480c593473e3ef98",
+    "url": "/React2/index.html"
+  },
+  {
+    "revision": "86820b3511bb041ef347",
+    "url": "/React2/static/js/2.018c1499.chunk.js"
+  },
+  {
+    "revision": "15ddef33ad4abcc93846979af382b4bb",
+    "url": "/React2/static/js/2.018c1499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500d71b45f04929fdac8",
+    "url": "/React2/static/js/main.78f4ca0b.chunk.js"
+  },
+  {
+    "revision": "c048c99054379ce031ad",
+    "url": "/React2/static/js/runtime-main.133e23bc.js"
+  }
+]);
